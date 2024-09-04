cfea162abd124c5554dd8ee76705a15182c035d8
Adrian Castañeda <adriansalvador456@gmail.com>
2024-09-04 11:41:59 -0700

    Se realizo el diseño del login

---
+++ app/src/main/java/com/example/aplicacioninventario/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.aplicacioninventario;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Elimina la funcionalidad de borde a borde para pruebas.
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(0, 0, 0, 0); // Ajusta el relleno según sea necesario.
+            return insets;
+        });
+    }
+}
